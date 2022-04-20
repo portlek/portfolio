3cd6a8a9db37c7f34903117481b1e71844301f91
Hasan Demirtaş <utsukushihito@outlook.com>
2022-04-21 01:01:15 +0300

    fix.

--- components/DarkToggle.vue
+++ components/DarkToggle.vue
@@ -1,12 +1,15 @@
 <script setup lang="ts">
 const color = useColorMode()
+
 function toggleDark() {
-  color.preference = color.preference === 'dark' ? 'light' : 'dark'
+  var isDark = color.preference === 'dark'
+  color.value = isDark ? 'light' : 'dark'
+  color.preference = isDark ? 'light' : 'dark'
 }
 </script>
 
 <template>
-  <button class="!outline-none" @click="toggleDark()">
+  <button class="!outline-none" @click="toggleDark">
     <div class="dark:i-carbon-moon i-carbon-sun" />
   </button>
 </template>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,15 +1,6 @@
-import path from 'path'
 import { defineNuxtConfig } from 'nuxt'
 
 export default defineNuxtConfig({
-  meta: {
-    title: 'Vitesse Nuxt 3',
-    link: [
-      {
-        rel: 'icon', type: 'image/png', href: '/nuxt.png',
-      },
-    ],
-  },
   modules: [
     '@vueuse/nuxt',
     '@unocss/nuxt',
@@ -17,12 +8,6 @@ export default defineNuxtConfig({
     '@nuxtjs/color-mode',
     '@nuxtjs/i18n',
   ],
-  autoImports: {
-    dirs: [
-      'composables',
-      'layouts',
-    ],
-  },
   experimental: {
     reactivityTransform: true,
   },
