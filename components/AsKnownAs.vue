01734b22e75405c5134d7b9502517eda1dd01e50
Hasan Demirtaş <utsukushihito@outlook.com>
2022-04-20 14:01:27 +0300

    test

---
+++ components/AsKnownAs.vue
@@ -0,0 +1,21 @@
+<script setup lang="ts">
+const route = useRoute()
+const user = useUserStore()
+
+watchEffect(() => {
+  user.setNewName(route.params.id as string)
+})
+</script>
+
+<template v-if="user.otherNames.length">
+  <p text-sm my-4>
+    <span op-50>Also as known as:</span>
+    <ul>
+      <li v-for="otherName in user.otherNames" :key="otherName">
+        <router-link :to="`/hi/${otherName}`" replace>
+          {{ otherName }}
+        </router-link>
+      </li>
+    </ul>
+  </p>
+</template>
--- pages/hi/[id].vue
+++ pages/hi/[id].vue
@@ -1,14 +1,9 @@
 <script setup lang="ts">
 const route = useRoute()
-const user = useUserStore()
 const name = route.params.id
 
-watchEffect(() => {
-  user.setNewName(route.params.id as string)
-})
-
 definePageMeta({
-  layout: 'custom',
+  layout: 'home',
 })
 </script>
 
@@ -22,18 +17,7 @@ definePageMeta({
       {{ name }}!
     </div>
 
-    <template v-if="user.otherNames.length">
-      <p text-sm my-4>
-        <span op-50>Also as known as:</span>
-        <ul>
-          <li v-for="otherName in user.otherNames" :key="otherName">
-            <router-link :to="`/hi/${otherName}`" replace>
-              {{ otherName }}
-            </router-link>
-          </li>
-        </ul>
-      </p>
-    </template>
+    <AsKnownAs />
 
     <Counter />
 
