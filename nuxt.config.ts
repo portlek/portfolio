a871b6b5f4cc6ec0be96530deaff8f3b1b4bfb3c
Hasan Demirtaş <utsukushihito@outlook.com>
2022-04-20 17:14:44 +0300

    remove preset.

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,4 @@
 import { defineNuxtConfig } from 'nuxt'
-import presetWind from '@unocss/preset-wind'
 
 export default defineNuxtConfig({
   meta: {
@@ -41,9 +40,6 @@ export default defineNuxtConfig({
         'btn',
         'px-4 py-1 rounded inline-block bg-teal-600 text-white cursor-pointer hover:bg-teal-700 disabled:cursor-default disabled:bg-gray-600 disabled:opacity-50',
       ],
-    ],
-    presets: [
-      presetWind,
     ],
   },
   css: [
