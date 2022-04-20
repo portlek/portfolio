7bfd8a5d4a50c4aa87620eb4f125309920d88989
Hasan Demirtaş <utsukushihito@outlook.com>
2022-04-20 15:35:17 +0300

    lint fix.

---
+++ components/HomeButton.vue
@@ -0,0 +1,7 @@
+<template>
+  <div>
+    <NuxtLink class="btn m-3 text-sm" to="/">
+      Back
+    </NuxtLink>
+  </div>
+</template>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,6 @@
-import { defineNuxtConfig } from 'nuxt3'
+import {
+  defineNuxtConfig,
+} from 'nuxt3'
 
 export default defineNuxtConfig({
   meta: {
@@ -19,11 +21,11 @@ export default defineNuxtConfig({
     dirs: [
       'composables',
       'store',
+      'layouts',
     ],
   },
   experimental: {
     reactivityTransform: true,
-    // viteNode: true,
   },
   vueuse: {
     ssrHandlers: true,
@@ -36,11 +38,14 @@ export default defineNuxtConfig({
       scale: 1.2,
     },
     shortcuts: [
-      ['btn', 'px-4 py-1 rounded inline-block bg-teal-600 text-white cursor-pointer hover:bg-teal-700 disabled:cursor-default disabled:bg-gray-600 disabled:opacity-50'],
+      [
+        'btn',
+        'px-4 py-1 rounded inline-block bg-teal-600 text-white cursor-pointer hover:bg-teal-700 disabled:cursor-default disabled:bg-gray-600 disabled:opacity-50',
+      ],
     ],
   },
   css: [
-    '~/styles/main.css'
+    '~/styles/main.css',
   ],
   colorMode: {
     classSuffix: '',
