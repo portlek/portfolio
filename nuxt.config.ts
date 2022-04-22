e59fecf837d2db4cf9aa17b872d3d656837fa720
Hasan Demirtaş <utsukushihito@outlook.com>
2022-04-22 21:09:54 +0300

    start portfolio.

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -4,7 +4,6 @@ export default defineNuxtConfig({
   srcDir: 'src',
   modules: [
     '@vueuse/nuxt',
-    '@unocss/nuxt',
     '@pinia/nuxt',
     '@nuxtjs/color-mode',
   ],
@@ -14,19 +13,6 @@ export default defineNuxtConfig({
   vueuse: {
     ssrHandlers: true,
   },
-  unocss: {
-    uno: true,
-    attributify: true,
-    preflight: true,
-    icons: {
-      scale: 1.2,
-    },
-    shortcuts: [
-      [
-        'btn',
-        'px-4 py-1 rounded inline-block bg-teal-600 text-white cursor-pointer hover:bg-teal-700 disabled:cursor-default disabled:bg-gray-600 disabled:opacity-50',
-      ],
-    ],
   },
   colorMode: {
     classSuffix: '',
