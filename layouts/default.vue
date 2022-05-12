69919f0f67fbcd10637d813fb9c0eedf0f728fa2
Hasan Demirtaş <utsukushihito@outlook.com>
2022-05-12 14:35:05 +0300

    remove files.

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,17 +1,5 @@
 <template>
-  <div
-    class="
-      lg:p-[15px] p-0
-      lg:before:portfolio-topleft
-      lg:after:top-auto
-      lg:after:bottom-0
-      lg:after:portfolio-topleft
-      w-auto
-      h-auto
-      overflow-visible
-      relative
-      text-[#8c8c8e]"
-  >
+  <div>
     <slot />
   </div>
 </template>
