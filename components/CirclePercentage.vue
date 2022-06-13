1c837074f2d15e00eabb38da29824c5b899c68fd
Hasan Demirtaş <utsukushihito@outlook.com>
2022-06-13 15:30:45 +0300

    attributify

--- components/CirclePercentage.vue
+++ components/CirclePercentage.vue
@@ -6,35 +6,35 @@ const props = defineProps<{
 
 <template>
   <div
-    class="
     relative
     text-center
-    text-#8c8c83
     w-75px
-    h-80px"
+    h-80px
+    class="
+    text-#8c8c83"
   >
     <div
-      class="
       absolute
-      w-100%
       h-50px
       mb-15px
-      text-#cacace
       text-center
-      top-0"
+      top-0
+      class="
+      w-100%
+      text-#cacace"
     />
     <h6
-      class="
       absolute
       h-15px
-      text-#ffffff
       text-center
-      w-100%
       text-13px
       font-400
       bottom-0
       right-0
-      left-0"
+      left-0
+      class="
+      w-100%
+      text-#ffffff"
     >
       {{ lang }}
     </h6>
